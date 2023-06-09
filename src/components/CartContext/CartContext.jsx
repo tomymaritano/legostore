d3d73f57a8bdb445b29939267388c74c51b42b81
craftzcode <tomymaritano@gmail.com>
2023-06-09 13:54:18 +0200

    modifications

--- src/components/CartContext/CartContext.jsx
+++ src/components/CartContext/CartContext.jsx
@@ -1,11 +1,11 @@
 import { createContext, useState } from "react";
 
 export const CartContext = createContext({
-  cart: [],
+  cart: []
 });
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([])
 
   console.log(cart);
 
@@ -15,14 +15,15 @@ export const CartProvider = ({ children }) => {
     } else {
       console.error("El producto ya fue agregado");
     }
-  };
+  }
   const removeItem = (itemId) => {
     const cartUpdated = cart.filter(prod => prod.id !== itemId);
-  };
+    setCart(cartUpdated)
+  }
 
   const clearCart = () => {
     setCart([]);
-  };
+  }
 
   const isInCart = (itemId) => {
     return cart.some(prod => prod.id === itemId);
